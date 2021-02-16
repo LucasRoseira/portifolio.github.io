4b328d6adbca6c9cbb6d9e13a96a7a685034f66a
Lucas <lucas.roseira@fatec.sp.gov.br>
2021-02-16 16:43:53 -0300

    Atualizações
    Atualização nos links do menu;
    Ano atual usando JS;
    Atualização nos textos e inclusão da Emancipa;
    Inclusão de Links nas tecnologias e linguagens;
    Inclusão do mapa;

--- assets/layout/scripts/script.js
+++ assets/layout/scripts/script.js
@@ -1,14 +1,18 @@
+
+document.getElementById("year").innerHTML = new Date().getFullYear();
+
 var slideIndex = 0;
 carousel();
 
 function carousel() {
-  var i;
-  var x = document.getElementsByClassName("mySlides");
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";
-  }
-  slideIndex++;
-  if (slideIndex > x.length) {slideIndex = 1}
-  x[slideIndex-1].style.display = "block";
-  setTimeout(carousel, 3500); // Change image every 2 seconds
+	var i;
+	var x = document.getElementsByClassName("mySlides");
+	for (i = 0; i < x.length; i++) {
+		x[i].style.display = "none";
+	}
+	slideIndex++;
+	if (slideIndex > x.length) {slideIndex = 1}
+		x[slideIndex-1].style.display = "block";
+	setTimeout(carousel, 3500);
 }
+
